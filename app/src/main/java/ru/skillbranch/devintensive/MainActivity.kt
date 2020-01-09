f94082681e9d5c1d41bc8a5514447cd0af12b1ae
kirill <k.zubarev@12.digital>
2020-01-09 09:53:10 +0300

    fix onClick

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -61,7 +61,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onResume() {
         super.onResume()
-//        this.hideKeyboard()
         Log.d("M_MainActivity", "onResume")
     }
 
@@ -94,8 +93,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        this.hideKeyboard()
+
         if (v?.id === R.id.iv_send) {
-            this.hideKeyboard()
             val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
             messageEt.setText("")
             val (r, g, b) = color
